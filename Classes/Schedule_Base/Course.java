8f2bc14f0d2353755dfc1bd0d107d58d3ad470f2
Cody Thompson <47369629+TrueSgtMonkey@users.noreply.github.com>
2020-04-10 23:14:15 -0700

    Update Course.java
    Split courseName into subject (a String) and courseNumber (an int).
    Added a course description, room, and instructor variables.
    Changed the printCourse() function to add an extra "0" before the time to print the times out better.
        (Like 8:00 instead of 8:0)

--- Classes/Schedule_Base/Course.java
+++ Classes/Schedule_Base/Course.java
@@ -1,22 +1,37 @@
 class Course
 {
-    private TimeSlot[] dayAndTime;
-    private String name;
+    private String subject;
+    private int courseNumber;
+    private String description;
     private int id;
+    private String room;
+    private String instructor;
+    private TimeSlot[] dayAndTime;
 
     /**
     * Course constructor.
-    * @param courseName String that contains course subject and number
-    *                   ex: "CSC 131"
+    * @param subject String that contains course subject
+    *                   ex: "CSC"
+    * @param courseNumber Integer course number
+    *                   ex: 131
     * @param courseId Integer unique ID for course
+    * @param description String describes what the course is
+    *                   ex: "Introduction to Programming Logic"
+    * @param room String that contains the course room
+    * @param instructor String contains professor's name for the course
+    *                   ex: "Devin Cook"
     * @param dayAndTime String that contains day and time data
     *                   ex: "MoWe 8:00AM - 9:00AM"
     */
-    Course(String courseName, int courseId, String dayAndTime)
+    Course(String subject, int courseNumber, String description, int courseId, String dayAndTime, String room, String instructor)
     {
-        this.name = courseName;
+        this.subject = subject;
+        this.courseNumber = courseNumber;
+        this.description = description;
         this.id = courseId;
         String[] dayAndTimeList = dayAndTime.split("\n");
+        this.room = room;
+        this.instructor = instructor;
         this.dayAndTime = new TimeSlot[dayAndTimeList.length];
         for(int i = 0; i < dayAndTimeList.length; i++)
         {
@@ -30,34 +45,59 @@ class Course
     */
     Course(Course course)
     {
-        this.name = course.name;
+        this.subject = course.subject;
+        this.courseNumber = course.courseNumber;
+        this.description = course.description;
         this.id = course.id;
+        this.room = course.room;
+        this.instructor = course.instructor;
         this.dayAndTime = new TimeSlot[course.dayAndTime.length];
         for(int i = 0; i < course.dayAndTime.length; i++)
         {
             this.dayAndTime[i] = new TimeSlot(course.dayAndTime[i]);
         }
     }
+    
+    /*
+     *      private TimeSlot[] dayAndTime;
+		    private String subject;
+		    private int courseNumber;
+		    private int id;
+		    private String room;
+		    private String instructor;
+     */
 
-    public TimeSlot[] getTimeSlots(){ return this.dayAndTime; }
+    public String getName(){ return this.subject; }
 
-    public String getName(){ return this.name; }
-
+    public int getNumber() { return this.courseNumber; }
+    
+    public String getDescription() { return this.description; }
+    
     public int getId(){ return this.id; }
+    
+    public TimeSlot[] getTimeSlots(){ return this.dayAndTime; }
+    
+    public String getRoom() { return this.room; }
+    
+    public String getTeacher() { return this.instructor; }
 
     /**
     * Prints out the Course information in the following format:
-    *   Name: CSC 131
+    *   subject: CSC131
     *   ID: 31407
+    *   description: Computer Software Engineering
     *   Days: Monday
     *         Wednesday
     *   Start Time: 17:30
     *   End Time: 18:45
+    *   Room: Eureka 121
+    *   Instructor: Jagannadha Chidella
     */
     public void printCourse()
     {
-        System.out.println("Name: " + this.name);
+        System.out.println("subject: " + this.subject + this.courseNumber);
         System.out.println("ID: " + this.id);
+        System.out.println("description: " + this.description);
         for(TimeSlot dayAndTime : this.dayAndTime)
         {
             String[] days = dayAndTime.getDays();
@@ -66,15 +106,22 @@ class Course
             {
                 System.out.println("      " + days[i]);
             }
+            String extraZero = "";
+            if(dayAndTime.getEndMin() < 10)
+            {
+                extraZero = "0";
+            }
             System.out.println("Start Time: " + 
                             dayAndTime.getStartHour() +
-                            ":" +
+                            ":" + extraZero +
                             dayAndTime.getStartMin());
             System.out.println("End Time: " + 
                             dayAndTime.getEndHour() +
-                            ":" +
+                            ":" + extraZero +
                             dayAndTime.getEndMin());
         }
+        System.out.println("Room: " + this.room);
+        System.out.println("Instructor: " + this.instructor);
     }
 
     /**
