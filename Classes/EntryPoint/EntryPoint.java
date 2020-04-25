dd0dd9c2165edd81e24bc80e576a0996005cad6c
kmkirby <kmkirby90@gmail.com>
2020-04-25 11:35:09 -0700

    added menu to EntryPoint

--- Classes/EntryPoint/EntryPoint.java
+++ Classes/EntryPoint/EntryPoint.java
@@ -8,8 +8,35 @@ public class EntryPoint
 
         String sPath = "jdbc:sqlite:C:/Users/naume/Documents/SQLite/Classes.db";
         SQLiteDB sql = new SQLiteDB(sPath);
-        sql.Execute("Select * from CourseList;", courseList);
+        
+        Scanner input = new Scanner(System.in);
+        int selection;
 
-        System.out.println("Import Successful.");
+        do
+        {
+            System.out.println("Select an option:");
+            System.out.println("1) Add course to schedule");
+            System.out.println("2) Print schedules");
+            selection = input.nextInt();
+            input.nextLine();//skip \n after int input.
+            if(selection == 1)
+            {
+                System.out.println("Enter subject:");
+                String subject = input.nextLine().toUpperCase();
+                System.out.println("Enter course number:");
+                String number = input.nextLine();
+                String query = "SELECT * FROM CourseList WHERE Subject='"+subject+"' AND Course_No='"+number+"';";
+                if(sql.Execute(query, courseList))
+                {
+                    System.out.println(subject + " " + number + " added to schedule");
+                    //Planner.addCourses(courseList)
+                }
+                else
+                {
+                    System.out.println("No courses matching: " + subject + " " + number);
+                }
+            }
+        }while(selection != 2);
+        //Planner.print()
     }
 }
